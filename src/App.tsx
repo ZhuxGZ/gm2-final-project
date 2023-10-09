fb13d9e6c0dea1c014fc889eb18f0418fffd4282
GM2 Academy <gm2academy@MBP-de-Tecnico-267.fibertel.com.ar>
2023-10-09 12:30:34 -0300

    feat:creation of the Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home } from './pages';
+import { Home, Login } from './pages';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
 			</Routes>
 		</BrowserRouter>
 	);
