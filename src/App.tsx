61b84ba99b73f08173da6a9fd283afbcb4153b9a
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-10-09 11:36:47 -0300

    fix: correct the children type on AppLayoutProps

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages';
+import { AppLayout } from './components';
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-			</Routes>
-		</BrowserRouter>
+		<AppLayout>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</BrowserRouter>
+		</AppLayout>
 	);
 }
 
