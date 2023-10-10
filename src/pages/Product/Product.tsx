5736bb54fe34cafa4e85df26e547c7750ff42d70
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-10-10 12:52:43 -0300

    feat(Product.tsx): created the page basic structure

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,3 +1,15 @@
+import { useParams } from 'react-router-dom';
 import './Product.css';
 
-export const Product = () => {};
+export const Product = () => {
+	const { id } = useParams();
+
+	return (
+		<section className="prod-container">
+			<section className="prod-image"></section>
+			<section className="prod-info">
+				<h3 className="prod-info-title">Prod Name</h3>
+			</section>
+		</section>
+	);
+};
