2251f9bdcd165884a9c3e5d1196df9f2a19c7dc6
LucasVidalle <lucas.vidalle@gm2dev.com>
2023-10-10 13:02:24 -0300

    feat: add card style

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,7 +4,7 @@ export const Card = () => {
     return (
         <>
             <div className="structure-card">
-                <div className="card-img&rating">
+                <div className="img-rating-container">
                     <img className="card-img" src="https://i.pinimg.com/564x/a1/58/1c/a1581ccebc3e55ab4502a0fc4102a3da.jpg" />
                     <p className="card-rating">#1</p>
                 </div>
