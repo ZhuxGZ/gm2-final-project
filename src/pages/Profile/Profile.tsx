c3f53c349857e2c6361b387f10dcad624cd3c221
GM2 Academy <gm2academy@MBP-de-Tecnico-323.fibertel.com.ar>
2023-10-10 12:57:37 -0300

    feat:fisnish the structure of the card profile

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,4 +1,15 @@
 import './Profile.css'
 export const Profile = () => {
-    return ()
+    return (
+        <div className='Card'>
+            <div className='contentCard'>
+                <img className='img-profile' src="https://avatargenerationsgame.b-cdn.net/wp-content/uploads/2023/01/ARES_fullbody_Aang_outfit1.png" alt="" />
+                <div className='info'>
+                    <h2>oli</h2>
+                    <h2>maestro</h2>
+                    <h2>licenciado</h2>
+                </div>
+            </div>
+        </div>
+    )
 }
