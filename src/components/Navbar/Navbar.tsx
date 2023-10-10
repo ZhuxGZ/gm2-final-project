7d04d8e8c060db9a49a3c853ae38bc6a6bf4c2c1
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-10-10 11:50:16 -0300

    feat: Add navlinks to the navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,11 @@
 import { NavBrand } from './NavBrand';
+import { NavLinks } from './NavLinks';
 import './Navbar.css';
 export const Navbar = () => {
 	return (
 		<nav>
 			<NavBrand />
+			<NavLinks />
 		</nav>
 	);
 };
