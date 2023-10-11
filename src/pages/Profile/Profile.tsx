4e9c8855f647872ffb771755a6fdf35f949d23d5
GM2 Academy <gm2academy@MBP-de-Tecnico.fibertel.com.ar>
2023-10-11 11:01:41 -0300

    feat:creation of card profile

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,13 +1,77 @@
 import './Profile.css'
+export interface Welcome {
+    id: number;
+    firstName: string;
+    lastName: string;
+    maidenName: string;
+    age: number;
+    gender: string;
+    email: string;
+    phone: string;
+    username: string;
+    password: string;
+    birthDate: Date;
+    image: string;
+    bloodGroup: string;
+    height: number;
+    weight: number;
+    eyeColor: string;
+    hair: Hair;
+    domain: string;
+    ip: string;
+    address: Address;
+    macAddress: string;
+    university: string;
+    bank: Bank;
+    company: Company;
+    ein: string;
+    ssn: string;
+    userAgent: string;
+}
+export interface Address {
+    address: string;
+    city: string;
+    coordinates: Coordinates;
+    postalCode: string;
+    state: string;
+}
+export interface Coordinates {
+    lat: number;
+    lng: number;
+}
+export interface Bank {
+    cardExpire: string;
+    cardNumber: string;
+    cardType: string;
+    currency: string;
+    iban: string;
+}
+export interface Company {
+    address: Address;
+    department: string;
+    name: string;
+    title: string;
+}
+export interface Hair {
+    color: string;
+    type: string;
+}
+
 export const Profile = () => {
     return (
         <div className='Card'>
             <div className='contentCard'>
-                <img className='img-profile' src="https://avatargenerationsgame.b-cdn.net/wp-content/uploads/2023/01/ARES_fullbody_Aang_outfit1.png" alt="" />
-                <div className='info'>
-                    <h2>oli</h2>
-                    <h2>maestro</h2>
-                    <h2>licenciado</h2>
+                <div className="post main">
+                    <div className="preview">
+                        <img className='img-profile' src="https://robohash.org/hicveldicta.png" alt="" />
+                    </div>
+                    <div className="details-profile">
+                        <h2>Username</h2>
+                        <h2>Age</h2>
+                        <h2>email</h2>
+                        <h2>phone number</h2>
+                        <h2>direction</h2>
+                    </div>
                 </div>
             </div>
         </div>
