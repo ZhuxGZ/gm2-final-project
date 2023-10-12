75c6bb20efa532df558c11ed0607422c0873cfb7
GM2 Academy <gm2academy@MBP-de-Tecnico-534.fibertel.com.ar>
2023-10-12 10:07:56 -0300

    fix:repair the sintaxis of the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD
-import { Home, Login, Profile } from './pages';
-=======
-import { Home, Login, Product } from './pages';
->>>>>>> 063e6b66421aa601840bd1daf3f1fc702cfd064b
+import { Home, Login, Product, Profile } from './pages';
+
 import { AppLayout } from './components';
 
 function App() {
@@ -14,15 +11,11 @@ function App() {
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/login" element={<Login />} />
-<<<<<<< HEAD
 					<Route path='/profile' element={<Profile />} />
-				</Routes >
-=======
 					<Route path="/product/:id" element={<Product />} />
 				</Routes>
->>>>>>> 063e6b66421aa601840bd1daf3f1fc702cfd064b
-			</AppLayout>
-		</BrowserRouter>
+			</AppLayout >
+		</BrowserRouter >
 	);
 }
 
