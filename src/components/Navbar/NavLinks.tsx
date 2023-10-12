c10e74a9d1a23e999748bd95bec3c12362acd935
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-10-12 11:59:24 -0300

    feat: Add the ability to change when is logged on

--- src/components/Navbar/NavLinks.tsx
+++ src/components/Navbar/NavLinks.tsx
@@ -1,12 +1,14 @@
 import { UserIcon } from '..';
 import { Link } from 'react-router-dom';
+import { useLoginStatus } from '../../hooks';
+import { useEffect } from 'react';
 
 export const NavLinks = () => {
-	const isLogged = true;
+	const { isLogged } = useLoginStatus();
 
 	return (
 		<ul className="nav-list">
-			{isLogged ?? (
+			{isLogged && (
 				<>
 					<li className="nav-item">Metrics</li>
 					<li className="nav-item">Cart</li>
