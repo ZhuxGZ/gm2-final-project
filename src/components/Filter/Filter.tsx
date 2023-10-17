b68885d70c725f8926741e84848d351f0cf898d3
GM2 Academy <gm2academy@MBP-de-Tecnico-629.fibertel.com.ar>
2023-10-17 12:55:54 -0300

    feat:creation structure of filter component

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,6 +1,25 @@
 import './Filter.css'
 export const Filter = () => {
     return (
-        
+        <div className='content-filter'>
+            <h2>Filters</h2>
+            <div className='filter-description container-filter'>
+                <h2>Name Descripton Brand</h2>
+                <input type="text" />
+            </div>
+            <div className='filter-minprice container-filter'>
+                <h2>Min Price</h2>
+                <input type="range" />
+            </div>
+            <div className='filter-maxprice container-filter'>
+                <h2>Max Price</h2>
+                <input type="range" />
+            </div>
+            <div className='filter-categories container-filter'>
+                <h2>Categories</h2>
+                <select name="saiufbasbdhjab" id=""><option value="aidufhiasdfiyghsuidfhuisadhfuishauidfh"></option><option value="penex2"></option></select>
+            </div>
+        </div>
+
     )
 }
