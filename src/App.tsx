131bb42632060801cf8cda5cc536cafd0922c127
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-10-18 10:51:24 -0300

    feat: Add the Cart Provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,24 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home, Login, Profile, Product, Cart } from './pages';
 import { AppLayout } from './components';
-import { LoginStatusProvider, ProductsProvider } from './hooks';
+import { CartProvider, LoginStatusProvider, ProductsProvider } from './hooks';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<LoginStatusProvider>
 				<ProductsProvider>
-					<AppLayout>
-						<Routes>
-							<Route path="/" element={<Home />} />
-							<Route path="/login" element={<Login />} />
-							<Route path="/product/:id" element={<Product />} />
-							<Route path="/profile" element={<Profile />} />
-							<Route path="/cart" element={<Cart />} />
-						</Routes>
-					</AppLayout>
+					<CartProvider>
+						<AppLayout>
+							<Routes>
+								<Route path="/" element={<Home />} />
+								<Route path="/login" element={<Login />} />
+								<Route path="/product/:id" element={<Product />} />
+								<Route path="/profile" element={<Profile />} />
+								<Route path="/cart" element={<Cart />} />
+							</Routes>
+						</AppLayout>
+					</CartProvider>
 				</ProductsProvider>
 			</LoginStatusProvider>
 		</BrowserRouter>
