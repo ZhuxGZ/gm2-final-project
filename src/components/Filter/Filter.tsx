910a69e2f42ed6fa9a7288ab0669cb8005809537
GM2 Academy <gm2academy@MBP-de-Tecnico-667.fibertel.com.ar>
2023-10-18 11:32:46 -0300

    feat:create of Filter Component

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,23 +1,26 @@
 import './Filter.css'
 export const Filter = () => {
     return (
+
         <div className='content-filter'>
-            <h2>Filters</h2>
-            <div className='filter-description container-filter'>
-                <h2>Name Descripton Brand</h2>
+            <div className='filter-title'>
+                <h2>Filters</h2>
+            </div>
+            <div className='filter-description container-filter filter-center'>
+                <h3>Name Descripton Brand</h3>
                 <input type="text" />
             </div>
-            <div className='filter-minprice container-filter'>
-                <h2>Min Price</h2>
+            <div className='filter-minprice container-filter filter-center'>
+                <h3>Min Price</h3>
                 <input type="range" />
             </div>
-            <div className='filter-maxprice container-filter'>
-                <h2>Max Price</h2>
+            <div className='filter-maxprice container-filter filter-center'>
+                <h3>Max Price</h3>
                 <input type="range" />
             </div>
-            <div className='filter-categories container-filter'>
-                <h2>Categories</h2>
-                <select name="saiufbasbdhjab" id=""><option value="aidufhiasdfiyghsuidfhuisadhfuishauidfh"></option><option value="penex2"></option></select>
+            <div className='filter-categories container-filter filter-center'>
+                <h3>Categories</h3>
+                <select name="" id=""><option value="">pene</option><option value="">penex2</option></select>
             </div>
         </div>
 
