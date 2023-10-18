f65bb86caee7e96398be545ff71ab7b7dcf9103f
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-10-18 09:50:26 -0300

    feat: Add structure and styling to the cart

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,5 +1,12 @@
 import './Cart.css';
 
 export const Cart = () => {
-	return <div className="cart-container"></div>;
+	return (
+		<div className="cart-container">
+			<div className="cart-top">
+				<h1 className="cart-title">Cart</h1>
+				<button className="cart-buy">Buy</button>
+			</div>
+		</div>
+	);
 };
