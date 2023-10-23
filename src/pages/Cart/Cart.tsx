dc9487c763902f9fb86fe87d66ff907c1f2e826a
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-10-23 12:14:49 -0300

    feat: Add total to the cart

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,14 +1,27 @@
 import { useCart } from '../../hooks';
 import { Card } from '../../components';
 import './Cart.css';
+import { calculateDiscount } from '../../utils';
+import { useEffect, useState } from 'react';
 
 export const Cart = () => {
 	const { cartList } = useCart();
-	console.log(cartList);
+	const [totalCart, setTotalCart] = useState(0);
+	useEffect(() => {
+		let total = 0;
+
+		cartList.forEach((product) => {
+			total += calculateDiscount(product.price, product.discountPercentage);
+		});
+
+		setTotalCart(Number(total.toFixed(2)));
+	}, [cartList]);
+
 	return (
 		<div className="cart-container">
 			<section className="cart-top">
 				<h1 className="cart-title">Cart</h1>
+				<h2 className="cart-total"> Total: ${totalCart}</h2>
 				<button className="cart-buy">Buy</button>
 			</section>
 			<section className="cart-product-display">
