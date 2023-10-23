073bd131aa78bc066a7454f3ebb567b0867e9662
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-10-23 10:26:51 -0300

    feat: Add filter by price

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,34 +1,60 @@
-import './Filter.css'
-import { useProducts } from '../../hooks';
+import { useState } from 'react';
+import './Filter.css';
 
+export const Filter = ({ setSearchText, setPriceMinMax }) => {
+	const [minPrice, setMinPrice] = useState(0);
+	const [maxPrice, setMaxPrice] = useState(2000);
 
-export const Filter = ({ setSearchText }) => {
+	function handleSearch(event) {
+		setSearchText(event.target.value);
+	}
 
-    function handleSearch(event) {
-        setSearchText(event.target.value)
-    }
-    return (
-        <div className='content-filter'>
-            <div className='filter-title'>
-                <h2>Filters</h2>
-            </div>
-            <div className='filter-description container-filter filter-center'>
-                <h3>Name Descripton Brand</h3>
-                <input type="text" placeholder='Search' onChange={handleSearch} />
-            </div>
-            <div className='filter-minprice container-filter filter-center'>
-                <h3>Min Price</h3>
-                <input type="range" />
-            </div>
-            <div className='filter-maxprice container-filter filter-center'>
-                <h3>Max Price</h3>
-                <input type="range" />
-            </div>
-            <div className='filter-categories container-filter filter-center'>
-                <h3>Categories</h3>
-                <select name="" id=""><option value="">hola</option><option value="">mundo</option></select>
-            </div>
-        </div>
+	const handlePriceQuantity = () => {
+		setPriceMinMax({ min: minPrice, max: maxPrice });
+	};
 
-    )
-}
+	return (
+		<div className="content-filter">
+			<div className="filter-title">
+				<h2>Filters</h2>
+			</div>
+			<div className="filter-description container-filter filter-center">
+				<h3>Name Descripton Brand</h3>
+				<input type="text" placeholder="Search" onChange={handleSearch} />
+			</div>
+			<div className="filter-minprice container-filter filter-center">
+				<h3>Min Price</h3>
+				<input
+					type="range"
+					min={0}
+					max={2000}
+					defaultValue={minPrice}
+					onChange={(event) => {
+						setMinPrice(Number(event.target.value));
+						handlePriceQuantity();
+					}}
+				/>
+			</div>
+			<div className="filter-maxprice container-filter filter-center">
+				<h3>Max Price</h3>
+				<input
+					type="range"
+					min={0}
+					max={2000}
+					defaultValue={maxPrice}
+					onChange={(event) => {
+						setMaxPrice(Number(event.target.value));
+						handlePriceQuantity();
+					}}
+				/>
+			</div>
+			<div className="filter-categories container-filter filter-center">
+				<h3>Categories</h3>
+				<select name="" id="">
+					<option value="">hola</option>
+					<option value="">mundo</option>
+				</select>
+			</div>
+		</div>
+	);
+};
--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -3,21 +3,26 @@ import { useState } from 'react';
 import { Filter } from '../../components';
 import { CardView } from '../../components/CardView';
 import { useProducts } from '../../hooks';
+import { calculateDiscount } from '../../utils';
 
 export const Shop = () => {
 	const [searchTerm, setSearchText] = useState<string>('');
+	const [priceMinMax, setPriceMinMax] = useState({ min: 0, max: 1000 });
+	console.log(priceMinMax.min, priceMinMax.max);
 
 	const products = useProducts();
 	const filteredProducts = products.filter(
 		(product) =>
-			product.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-			product.brand.toLowerCase().includes(searchTerm.toLowerCase())
+			(product.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+				product.brand.toLowerCase().includes(searchTerm.toLowerCase())) &&
+			calculateDiscount(product.price, product.discountPercentage) > priceMinMax.min &&
+			calculateDiscount(product.price, product.discountPercentage) < priceMinMax.max
 	);
 
 	console.log(filteredProducts);
 	return (
 		<section className="shop-page">
-			<Filter setSearchText={setSearchText} />
+			<Filter setSearchText={setSearchText} setPriceMinMax={setPriceMinMax} />
 			<CardView filteredProducts={filteredProducts} />
 		</section>
 	);
