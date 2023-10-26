f358665259ce602c2d463526746e9de35d60fb78
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-10-26 15:00:06 -0300

    feat(burgerMenu): Add the menuburger function

--- src/components/Navbar/NavLinks.tsx
+++ src/components/Navbar/NavLinks.tsx
@@ -2,37 +2,50 @@ import { UserIcon } from '..';
 import { Link } from 'react-router-dom';
 import { useCart, useLoginStatus } from '../../hooks';
 import { CartSvg } from './CartSvg';
+import { BurgerMenuSvg } from '../BurgerMenuSvg/BurgerMenuSvg';
 
-export const NavLinks = () => {
+export const NavLinks = (props) => {
 	const { isLogged } = useLoginStatus();
 	const { cartQuantity } = useCart();
+	const { menuState, setMenuState } = props;
+	const handleClick = () => {
+		console.log(setMenuState(!menuState));
+	};
 
 	return (
-		<ul className="nav-list">
-			{isLogged && (
-				<>
-					<li className="nav-item">Metrics</li>
-					<li className="nav-item">
-						<Link to="/wishlist">Whislist</Link>
-					</li>
-				</>
-			)}
-			<li className="nav-item">
-				<Link to="/shop">Shop</Link>
-			</li>
-
-			{isLogged && (
-				<li className="nav-item nav-cart">
-					<Link to="/cart">
-						<CartSvg />
-						<span className="nav-cartQuantity">{cartQuantity}</span>
-					</Link>
+		<>
+			<ul className="nav-list">
+				{isLogged && menuState && (
+					<>
+						<li className="nav-item">Metrics</li>
+						<li className="nav-item">
+							<Link to="/wishlist">Whislist</Link>
+						</li>
+					</>
+				)}
+				<li className="nav-item">
+					<Link to="/shop">Shop</Link>
 				</li>
-			)}
 
-			<Link to={isLogged ? '/profile' : '/login'}>
-				<UserIcon />
-			</Link>
-		</ul>
+				{isLogged && menuState && (
+					<li className="nav-item nav-cart">
+						<Link to="/cart">
+							<CartSvg />
+							<span className="nav-cartQuantity">{cartQuantity}</span>
+						</Link>
+					</li>
+				)}
+			</ul>
+			<ul className="user-menu">
+				{isLogged && (
+					<button className="burger-menu" onClick={handleClick}>
+						<BurgerMenuSvg open={false} />
+					</button>
+				)}
+				<Link to={isLogged ? '/profile' : '/login'}>
+					<UserIcon />
+				</Link>
+			</ul>
+		</>
 	);
 };
